ee1ac07a78f4d8b60d4fa0ba5e6caaec342cc01b
April Sharp <oyoaagent1@gmail.com>
2023-11-06 15:24:46 -0800

    added some placeholder assets and finished player movement + controler support, also changed tilesize to 32

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,46 @@
 extends Area2D
 
-var move_inputs: InputMap
+var move_inputs: Vector2
+
+@export var tile_size: int = 16
+@export var speed = 0.1
+var moving: bool = false
+var tween: Tween
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tween = create_tween()
 
+func on_tween_finished():
+	moving = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	_get_input()
+	move(move_inputs)
+
+func _get_input():
+	move_inputs.x = Input.get_axis("Move Left ", "Move Right")
+	move_inputs.y = Input.get_axis("Move Up", "Move Down")
+	if move_inputs.y != 0:
+		move_inputs.x = 0
+
+	if move_inputs.x < 0:
+		move_inputs.x = -1
+	if move_inputs.x > 0:
+		move_inputs.x = 1
+	if move_inputs.y < 0:
+		move_inputs.y = -1
+	if move_inputs.y > 0:
+		move_inputs.y = 1
+
+func move(amount: Vector2):
+	if amount == Vector2.ZERO:
+		return
+	if !moving:
+		var new_pos: Vector2
+		new_pos = position + amount * tile_size
+		print(position, new_pos)
+		moving = true
+		tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(self, "position", new_pos, speed)
+		tween.connect("finished", on_tween_finished)
